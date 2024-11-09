8f22113d5b65a5e858e1be852b09b898d7773260
petra <tf3d0tova@yandex.ru>
2024-11-09 14:27:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace LR6_TVis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.loadIm_Btn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.loadVid_Btn = new System.Windows.Forms.ToolStripButton();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.mapBox = new System.Windows.Forms.PictureBox();
+            this.btn_Outline = new System.Windows.Forms.Button();
+            this.coordsListBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fShiftBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rShiftBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rotationBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadIm_Btn,
+            this.toolStripSeparator1,
+            this.loadVid_Btn});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(965, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // loadIm_Btn
+            // 
+            this.loadIm_Btn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.loadIm_Btn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadIm_Btn.Name = "loadIm_Btn";
+            this.loadIm_Btn.Size = new System.Drawing.Size(55, 24);
+            this.loadIm_Btn.Text = "Image";
+            this.loadIm_Btn.Click += new System.EventHandler(this.loadIm_Btn_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // loadVid_Btn
+            // 
+            this.loadVid_Btn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.loadVid_Btn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadVid_Btn.Name = "loadVid_Btn";
+            this.loadVid_Btn.Size = new System.Drawing.Size(52, 24);
+            this.loadVid_Btn.Text = "Video";
+            this.loadVid_Btn.Click += new System.EventHandler(this.loadVid_Btn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // mapBox
+            // 
+            this.mapBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mapBox.Location = new System.Drawing.Point(224, 42);
+            this.mapBox.Name = "mapBox";
+            this.mapBox.Size = new System.Drawing.Size(729, 400);
+            this.mapBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mapBox.TabIndex = 23;
+            this.mapBox.TabStop = false;
+            // 
+            // btn_Outline
+            // 
+            this.btn_Outline.Location = new System.Drawing.Point(15, 305);
+            this.btn_Outline.Name = "btn_Outline";
+            this.btn_Outline.Size = new System.Drawing.Size(189, 32);
+            this.btn_Outline.TabIndex = 21;
+            this.btn_Outline.Text = "DETECT";
+            this.btn_Outline.UseVisualStyleBackColor = true;
+            this.btn_Outline.Click += new System.EventHandler(this.btn_Outline_Click);
+            // 
+            // coordsListBox
+            // 
+            this.coordsListBox.FormattingEnabled = true;
+            this.coordsListBox.ItemHeight = 16;
+            this.coordsListBox.Items.AddRange(new object[] {
+            "0 25 100",
+            "1 25 25",
+            "2 260 25",
+            "3 430 25",
+            "4 520 75",
+            "5 520 275"});
+            this.coordsListBox.Location = new System.Drawing.Point(15, 42);
+            this.coordsListBox.Name = "coordsListBox";
+            this.coordsListBox.Size = new System.Drawing.Size(189, 100);
+            this.coordsListBox.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Height";
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(14, 45);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(58, 22);
+            this.heightBox.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Forward Shift";
+            // 
+            // fShiftBox
+            // 
+            this.fShiftBox.Location = new System.Drawing.Point(96, 45);
+            this.fShiftBox.Name = "fShiftBox";
+            this.fShiftBox.Size = new System.Drawing.Size(58, 22);
+            this.fShiftBox.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Right Shift";
+            // 
+            // rShiftBox
+            // 
+            this.rShiftBox.Location = new System.Drawing.Point(96, 87);
+            this.rShiftBox.Name = "rShiftBox";
+            this.rShiftBox.Size = new System.Drawing.Size(58, 22);
+            this.rShiftBox.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 16);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Rotation";
+            // 
+            // rotationBox
+            // 
+            this.rotationBox.Location = new System.Drawing.Point(14, 89);
+            this.rotationBox.Name = "rotationBox";
+            this.rotationBox.Size = new System.Drawing.Size(58, 22);
+            this.rotationBox.TabIndex = 30;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rShiftBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.heightBox);
+            this.groupBox1.Controls.Add(this.rotationBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.fShiftBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 161);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 129);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camera Parameters";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 459);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.mapBox);
+            this.Controls.Add(this.btn_Outline);
+            this.Controls.Add(this.coordsListBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "FedotovaTA_221-328";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton loadIm_Btn;
+        private System.Windows.Forms.ToolStripButton loadVid_Btn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox mapBox;
+        private System.Windows.Forms.Button btn_Outline;
+        private System.Windows.Forms.ListBox coordsListBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fShiftBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rShiftBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rotationBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LR6_TVis
+{
+    public partial class Form1 : Form
+    {
+        List<CvRect> ObjsList = new List<CvRect>();
+        List<List<double>> objListForPaint = new List<List<double>>();
+        int[,] coordinates = new int[6, 3];//массив координат всех маркеров, на теретории поля, 1 столбец - номер маркера, потом х и у 
+        IplImage frame = null; // Буфер для хранения изображения
+        CvCapture capture = null; // Устройство захвата изображений
+        CvWindow windowCapture = new CvWindow("Video_Capture"); // Создаем окно
+        public Form1()
+        {
+            InitializeComponent();
+            heightBox.Text = "20";
+            rotationBox.Text = "15";
+            fShiftBox.Text = "5";
+            rShiftBox.Text = "5";
+        }
+
+        private void loadIm_Btn_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                frame = Cv.LoadImage(openFileDialog.FileName);
+                windowCapture.ShowImage(frame);
+            }
+            else
+            {
+                MessageBox.Show("[!] Error: can't open image file.");
+            }
+        }
+
+        private void loadVid_Btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Video Files|*.mp4;*.avi|All Files|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                capture = CvCapture.FromFile(openFileDialog.FileName);
+            }
+            else
+            {
+                capture = CvCapture.FromCamera(0); // Открытие камеры по умолчанию
+            }
+            if (capture != null)
+            {
+                timer.Start();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            frame = capture.QueryFrame();
+            if (frame != null)
+            {
+                windowCapture.ShowImage(objectsFounder());
+            }
+        }
+
+        private void btn_Outline_Click(object sender, EventArgs e)
+        {
+            windowCapture.ShowImage(objectsFounder());
+        }
+
+        private IplImage objectsFounder()
+        {
+            objListForPaint.Clear();
+            IplImage detected_obj_im = frame.Clone();
+            IplImage frameCL = frame.Clone();
+            List<List<int>> objects = new List<List<int>>();
+            bool good_object = true;
+
+            // Бинаризация изображения
+            IplImage black = new IplImage(frameCL.Size, BitDepth.U8, 1);
+            Cv.InRangeS(frame, new CvScalar(0, 0, 0), new CvScalar(180, 255, 60), black);
+
+            // Найдите контуры
+            CvSeq<CvPoint> contours;
+            CvMemStorage storage = new CvMemStorage();
+            Cv.FindContours(black, storage, out contours);
+            int minSize = 25; // минимальная сторона
+            int maxSize = 300; // максимальная сторона
+            for (CvSeq<CvPoint> current = contours; current != null; current = current.HNext)
+            {
+                good_object = true;
+                CvRect rect = Cv.BoundingRect(current);
+                int x = rect.X + rect.Width / 2;
+                int y = rect.Y + rect.Height / 2;
+
+                if (rect.Height < minSize || rect.Height > maxSize || rect.Width > rect.Height * 1.5 || rect.Height > rect.Width * 10)
+                    continue;
+
+                foreach (List<int> obj in objects.ToList())
+                {
+                    if (Math.Abs(obj[0] - x) <= 10 && Math.Abs(obj[1] - y) <= 10)
+                    {
+                        if (obj[2] < rect.Height) { objects.Remove(obj); }
+                        else { good_object = false; }
+                    }
+                }
+
+                if (good_object)
+                {
+                    List<int> center = new List<int>(); //координаты центра объекта
+                    center.Add(rect.X + rect.Width / 2);
+                    center.Add(rect.Y + rect.Height / 2);
+                    center.Add(rect.Height);
+                    center.Add(rect.Width);
+                    objects.Add(center);
+                }
+
+                //Cv.DrawContours(detected_obj_im, current, CvColor.Green, CvColor.Red, 1, 1, LineType.Link8);
+                //detected_obj_im.Rectangle(rect, CvColor.Blue, 2);
+                //CompareWithTemplate(rect, detected_obj_im);
+            }
+
+            foreach (List<int> obj in objects)
+            {
+                CvRect rect = new CvRect(obj[0] - obj[3] / 2, obj[1] - obj[2] / 2, obj[3], obj[2]);
+                IplImage frame_clone = frame.Clone();
+                CompareWithTemplate(rect, frame_clone);
+            }
+            foreach (List<double> infObj in objListForPaint)
+            {
+                int width = (int)infObj[6];
+                int height = (int)infObj[7];
+                int x = (int)infObj[4];
+                int y = (int)infObj[5];
+                int x_center = x + width / 2;
+                int y_center = y + height / 2;
+                double percant = infObj[3];
+                detected_obj_im.DrawLine((frame.Width / 2), frame.Height, x_center, y_center, CvColor.DarkGreen, 2); //gipotenuza
+                detected_obj_im.DrawLine((frame.Width / 2), frame.Height, (frame.Width / 2), y_center, CvColor.DarkGreen, 2); //vertical
+                detected_obj_im.DrawLine((frame.Width / 2), y_center, x_center, y_center, CvColor.DarkGreen, 2); //horizontal
+                int i = (int)infObj[0];
+                detected_obj_im.PutText($"Match: {percant:F2}%, {i}", new CvPoint(x, y - 10), new CvFont(FontFace.HersheySimplex, 0.4, 0.4, 0, 1, LineType.AntiAlias), CvColor.Red);
+                detected_obj_im.Rectangle(new CvRect(x, y, width, height), CvColor.Red, 2);
+            }
+            /*foreach (CvRect obj in ObjsList)
+            {
+                detected_obj_im.Rectangle(obj, CvColor.Red, 2);
+            }*/
+
+            // Задача 5: Вычисление положения и ориентации камеры по парам маркеров
+            //var markerPairs = GetMarkerPairs(objects);
+            //CalculateCameraPositionAndOrientation(markerPairs);
+
+            // Задача 7: Отрисовка поля, маркеров, робота и камеры
+            using (Graphics gr = mapBox.CreateGraphics())
+            {
+                if (objListForPaint.Any()) { DrawField(gr, objListForPaint, frame); }
+            }
+
+            return detected_obj_im;
+            //windowCapture.ShowImage(detected_obj_im);
+        }
+
+        private void CompareWithTemplate(CvRect rect, IplImage frameWithTemplate)
+        {
+            double width = rect.Width;
+            double height = rect.Height;
+            double x = rect.X + rect.Width / 2;
+            double y = rect.Y + rect.Height / 2;
+            int best_compare = 0;
+            double x_center = x + width / 2;
+            double y_center = y + height / 2;
+            double gipotenuza = Math.Sqrt(Math.Pow((double)x_center - (frame.Width / 2), 2) + Math.Pow((frame.Height / 2) - (double)y_center, 2));
+            double dx = ((double)x_center - (frame.Width / 2)) / (double)height;
+            //double gipotenuza = Math.Sqrt(Math.Pow((320 - x), 2) + Math.Pow((480 - y), 2));
+            float angle = (float)Math.Acos(((frame.Height / 2) - (double)y_center) / gipotenuza);
+            double matchPercentage = 0;
+            double best_percentage = 65;
+            bool found = false;
+            //List<double> percentages = new List<double> ();
+            //percentages.Add(50);
+            for (int i = 0; i < 6; i++)
+            {
+                // Установка ROI и копирование фрагмента изображения
+                frameWithTemplate.SetROI(rect);
+                IplImage fragment = frameWithTemplate.Clone();
+                frameWithTemplate.ResetROI();
+
+                IplImage maskImage = Cv.LoadImage($"C:\\Users\\fedot\\source\\repos\\tvision\\LR6_TVis\\temps\\{i}.png");
+
+                // Изменение размера фрагмента до размера шаблона
+                IplImage resizedFragment = new IplImage(maskImage.Size, maskImage.Depth, maskImage.NChannels);
+                Cv.Resize(fragment, resizedFragment, Interpolation.Linear);
+
+                IplImage black = new IplImage(maskImage.Size, BitDepth.U8, 1);
+
+                // Пороговые значения для красного цвета
+                Cv.InRangeS(resizedFragment, new CvScalar(0, 0, 0), new CvScalar(180, 255, 60), black);
+
+                // Преобразование maskImage в grayscale и изменение размера до размера шаблона, если требуется
+                IplImage resizedMaskImage = new IplImage(maskImage.Size, BitDepth.U8, 1);
+                Cv.CvtColor(maskImage, resizedMaskImage, ColorConversion.BgrToGray);
+
+                // Преобразование в бинарное изображение
+                IplImage binaryImage = new IplImage(resizedMaskImage.Size, BitDepth.U8, 1);
+                Cv.Threshold(resizedMaskImage, binaryImage, 128, 255, ThresholdType.Binary);
+
+                Bitmap black_im = black.ToBitmap();
+                Bitmap binaryImage_im = binaryImage.ToBitmap();
+                int compare_pix = 0;
+                int all_pix = 0;
+
+                // Попиксельное сравнение с шаблоном с учетом маски
+                IplImage result = new IplImage(maskImage.Size, BitDepth.U8, 1);
+                Cv.Xor(black, binaryImage, result);
+                Cv.Not(result, result);
+                // Вычисление количества белых пикселей                
+                all_pix = result.Width * result.Height;
+                compare_pix = Cv.CountNonZero(result);
+
+                matchPercentage = (double)compare_pix / all_pix * 100;
+
+                // Вывод результата
+                if (matchPercentage > best_percentage)
+                {
+                    best_compare = i;
+                    best_percentage = matchPercentage;
+                    //percentages.Add(matchPercentage);
+                    found = true;
+                }
+            }
+            if (found)
+            {
+                if (objListForPaint.Any())
+                {
+                    bool victory = true;
+                    foreach (List<double> infObj in objListForPaint.ToList())
+                    {
+                        if (infObj[0] == best_compare && infObj[3] < best_percentage)
+                        {
+                            objListForPaint.Remove(infObj);
+                            List<double> infoObj = new List<double>();
+                        }
+                        else if (infObj[0] == best_compare && infObj[3] >= best_percentage) { victory = false; }
+                    }
+                    if (victory)
+                    {
+                        List<double> infoObj = new List<double>();
+                        infoObj.Add(best_compare); //0
+                        infoObj.Add(gipotenuza);
+                        infoObj.Add(angle);
+                        infoObj.Add(best_percentage); //3
+                        infoObj.Add(rect.X);
+                        infoObj.Add(rect.Y);
+                        infoObj.Add(rect.Width); //6
+                        infoObj.Add(rect.Height);
+                        infoObj.Add(dx);
+                        objListForPaint.Add(infoObj);
+                    }
+                }
+                else
+                {
+                    List<double> infoObj = new List<double>();
+                    infoObj.Add(best_compare); //0
+                    infoObj.Add(gipotenuza);
+                    infoObj.Add(angle);
+                    infoObj.Add(best_percentage); //3
+                    infoObj.Add(rect.X);
+                    infoObj.Add(rect.Y);
+                    infoObj.Add(rect.Width); //6
+                    infoObj.Add(rect.Height);
+                    infoObj.Add(dx);
+
+                    objListForPaint.Add(infoObj);
+                }
+            }
+        }
+
+        private void DrawField(Graphics gr, List<List<double>> params_objects, IplImage frame)
+        {
+            int height = int.Parse(heightBox.Text.ToString());
+            int rotation = int.Parse(rotationBox.Text.ToString());
+            int rShift = int.Parse(rShiftBox.Text.ToString());
+            int fShift = int.Parse(fShiftBox.Text.ToString());
+            // Отрисовка поля
+            gr.Clear(Color.White);
+
+            for (int i = 0; i < 6; i++)
+            {
+                int x = int.Parse(coordsListBox.Items[i].ToString().Split(' ')[1]);
+                int y = int.Parse(coordsListBox.Items[i].ToString().Split(' ')[2]);
+                gr.FillEllipse(Brushes.Red, x, y, 6, 6);
+            }
+            double min_dx = 99999999;
+            int n = 0;
+            foreach (List<double> obj in params_objects)
+            {
+                if (Math.Abs(obj[8]) < min_dx)
+                {
+                    min_dx = obj[8];
+                    n = params_objects.IndexOf(obj);
+                }
+            }
+            int ii = (int)params_objects[n][0];
+            int x1 = int.Parse(coordsListBox.Items[(int)params_objects[n][0]].ToString().Split(' ')[1]);
+            int y1 = int.Parse(coordsListBox.Items[(int)params_objects[n][0]].ToString().Split(' ')[2]);
+            double g1 = params_objects[n][1];
+            double a1 = params_objects[n][2];
+            double dx = params_objects[n][8];
+            double g = 5000 / params_objects[n][7];
+
+            if (ii == 0)
+            {
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, y1, x1 + 3, (int)Math.Round(y1 + dx * 20)); //hor
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, (int)Math.Round(y1 + dx * 20), (int)Math.Round(x1 - Math.Cos(a1) * g1), (int)Math.Round(y1 + dx * 20)); //vert
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, y1, (int)Math.Round(x1 - Math.Cos(a1) * g1), (int)Math.Round(y1 + dx * 20)); //gip
+                gr.FillEllipse(new SolidBrush(Color.DarkBlue), (int)Math.Round(x1 - Math.Cos(a1) * g1), (int)Math.Round(y1 + dx * 20) - 10, 20, 20);
+            }
+            else if (ii == 4 || ii == 5)
+            {
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, y1, x1 + 3, (int)Math.Round(y1 - dx * 20)); //hor
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, (int)Math.Round(y1 - dx * 20), (int)Math.Round(x1 + Math.Cos(a1) * g1), (int)Math.Round(y1 - dx * 20)); //vert
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, y1, (int)Math.Round(x1 + Math.Cos(a1) * g1), (int)Math.Round(y1 - dx * 20)); //gip
+                gr.FillEllipse(new SolidBrush(Color.DarkBlue), (int)Math.Round(x1 + Math.Cos(a1) * g1) - 10, (int)Math.Round(y1 - dx * 20) - 10, 20, 20);
+            }
+            else
+            {
+                gr.DrawLine(new Pen(Color.Red), x1, y1 + 3, (int)Math.Round(x1 - dx * 20), y1 + 3); //hor
+                gr.DrawLine(new Pen(Color.Red), x1 + 3, y1 + 3, (int)Math.Round(x1 - dx * 20), (int)(y1 - Math.Cos(a1) * g1)); // gip
+                gr.DrawLine(new Pen(Color.Red), (int)Math.Round(x1 - dx * 20), y1 + 3, (int)Math.Round(x1 - dx * 20), (int)(y1 - Math.Cos(a1) * g1)); //vert
+                gr.FillEllipse(new SolidBrush(Color.DarkBlue), (int)Math.Round(x1 - dx * 20) - 10, (int)(y1 - Math.Cos(a1) * g1), 20, 20);
+            }
+            //int dx = x1 + int.Parse(Math.Round(Math.Sin(a) * g).ToString()); //horizontal
+            //int dy = y1 + int.Parse(Math.Round(Math.Cos(a) * g).ToString()); //vertical
+            //gr.FillEllipse(Brushes.DarkBlue, dx, dy, 20, 20);
+        }
+    }
+}
